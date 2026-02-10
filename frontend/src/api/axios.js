91bd1add927a5db265a1394f1ab660f357aeb71a
Al Amin Hossain <abirnayek31@gmail.com>
2026-02-11 02:37:50 +0600

    fixing backend url directly

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL:"https://task-contact-manager.onrender.com/api"
 });
 
 // Token attach 
